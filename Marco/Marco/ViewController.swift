cac6190052f81e5077263a8c786b2b34a427ecbe
Diego <A00827847@itesm.mx>
2021-09-06 18:34:00 -0500

    Se creó workspace

---
+++ Marco/Marco/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Marco
+//
+//  Created by Diego Solis on 9/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
