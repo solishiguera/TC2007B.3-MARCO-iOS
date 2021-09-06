307f85e240fda42a289db32ff9e0f053e3312a4e
Diego <A00827847@itesm.mx>
2021-09-06 18:38:21 -0500

    Marco test

--- Marco/Marco/ViewController.swift
+++ Marco/Marco/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         // Branch diego! :)
+        
+        // Branch marco!
     }
 
 
