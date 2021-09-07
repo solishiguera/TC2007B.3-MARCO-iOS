94ef759ad76c2e043de8827410361d4cd3f27ac9
A01570150 <A01570150@itesm.mx>
2021-09-06 20:46:07 -0500

    Sign up screen

---
+++ Marco/Marco/Controller/SignUpViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SignUpViewController.swift
+//  Marco
+//
+//  Created by Andres Piñones on 06/09/21.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
