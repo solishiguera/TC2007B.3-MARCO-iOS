ef845c3cae3d7f94b79c4809df8d1e4daaea81a7
Diego <A00827847@itesm.mx>
2021-09-08 07:45:18 -0500

    Added hide bar function

--- Marco/Marco/Controller/ViewController.swift
+++ Marco/Marco/Controller/ViewController.swift
@@ -30,10 +30,31 @@ class ViewController: UIViewController {
         loginBttn.layer.cornerRadius = 5
         signupBttn.layer.cornerRadius = 5
         guestBttn.layer.cornerRadius = 5
-
+        //Looks for single or multiple taps.
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
         
+        //Uncomment the line below if you want the tap not not interfere and cancel other interactions.
+        tap.cancelsTouchesInView = false
+        
+        view.addGestureRecognizer(tap)
+        viewWillDisappear(false)
     }
     
+    //Calls this function when the tap is recognized.
+    @objc func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
     
     @IBAction func signUpButtonTapped(_ sender: Any) {
         let signUpViewController = SignUpViewController(nibName: "SignUpViewController", bundle: nil)
@@ -51,8 +72,8 @@ class ViewController: UIViewController {
             Auth.auth().signIn(withEmail: correo, password: password) { result, err in
                 if err != nil {
                     self.errorLabel.text = "Datos Incorrectos"
-                }else{
-                    //ir a pantalla inicio
+                } else {
+                    // ir a pantalla inicio
                     self.errorLabel.text = "Logged In"
                 }
             }
