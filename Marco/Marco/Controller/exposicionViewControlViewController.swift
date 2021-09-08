07cca0d7ced044aaafd07933d654989d6feeb3ed
user193493 <user193493@74-80-245-22.ic2net.net>
2021-09-08 05:57:47 -0700

    Created exposicion

---
+++ Marco/Marco/Controller/exposicionViewControlViewController.swift
@@ -0,0 +1,29 @@
+//
+//  exposicionViewControlViewController.swift
+//  Marco
+//
+//  Created by user193493 on 9/8/21.
+//
+
+import UIKit
+
+class exposicionViewControlViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
