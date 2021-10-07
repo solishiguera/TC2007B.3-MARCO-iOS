a550a672e0aa93e1957fba96bbaf48809f535287
Diego <A00827847@itesm.mx>
2021-10-06 20:21:48 -0500

    Added tabbar elements

--- Marco/Marco/Controller/TabBarViewController.swift
+++ Marco/Marco/Controller/TabBarViewController.swift
@@ -8,42 +8,50 @@
 import UIKit
 
 class TabBarViewController: UITabBarController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let homeViewController = HomeViewController(nibName: "HomeViewController", bundle: nil)
         let exposicionViewController = exposicionViewControlViewController(nibName: "exposicionViewControlViewController", bundle: nil)
-        let controllers = [homeViewController, exposicionViewController]
+        let ticketViewController = TicketViewController(nibName: "TicketViewController", bundle: nil)
+        let restaurantViewController = RestaurantViewController(nibName: "RestaurantViewController", bundle: nil)
+        
+        let controllers = [homeViewController, exposicionViewController, ticketViewController, restaurantViewController]
         
         homeViewController.tabBarItem = UITabBarItem(
             title: "Home",
             image: UIImage(systemName: "house"),
             tag: 1)
         
-        exposicionViewController.tabBarItem = UITabBarItem(
-            title: "Exposiciones",
-            image: UIImage(systemName: "paintbrush"),
+        restaurantViewController.tabBarItem = UITabBarItem(
+            title: "Restaurante",
+            image: UIImage(systemName: "fork.knife"),
             tag: 2)
+        
+        ticketViewController.tabBarItem = UITabBarItem(
+            title: "Boletos",
+            image: UIImage(systemName: "ticket"),
+            tag: 3)
         
         exposicionViewController.tabBarItem = UITabBarItem(
             title: "Exposiciones",
             image: UIImage(systemName: "paintbrush"),
-            tag: 2)
-        
+            tag: 4)
+                
         self.viewControllers = controllers
         
         //UIApplication.shared.windows.first?.rootViewController = tabBarController
+        }
+        
+        
+        /*
+         // MARK: - Navigation
+         
+         // In a storyboard-based application, you will often want to do a little preparation before navigation
+         override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+         // Get the new view controller using segue.destination.
+         // Pass the selected object to the new view controller.
+         }
+         */
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
