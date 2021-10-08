ca227eda84d4d742e22e73daa663a72a22d5af59
Diego <A00827847@itesm.mx>
2021-10-07 19:45:15 -0500

    cleaning

--- Marco/Marco/Controller/exposicionViewControlViewController.swift
+++ Marco/Marco/Controller/exposicionViewControlViewController.swift
@@ -9,22 +9,8 @@ import UIKit
 
 class exposicionViewControlViewController: UIViewController {
     
-    @IBOutlet var bttnMenu: UIButton!
-    @IBOutlet var bttnBoletos: UIButton!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        viewWillDisappear(false)
-        
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        navigationController?.setNavigationBarHidden(true, animated: animated)
-    }
-
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        navigationController?.setNavigationBarHidden(false, animated: animated)
-    }
 }
