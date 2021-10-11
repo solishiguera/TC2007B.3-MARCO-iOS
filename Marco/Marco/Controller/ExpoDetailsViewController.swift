62e3bd8866eabe60714983704552dd124106791d
Diego <A00827847@itesm.mx>
2021-10-11 08:56:55 -0500

    front-end

--- Marco/Marco/Controller/ExpoDetailsViewController.swift
+++ Marco/Marco/Controller/ExpoDetailsViewController.swift
@@ -12,12 +12,18 @@ class ExpoDetailsViewController: UIViewController {
 
     @IBOutlet weak var titleLbl: UILabel!
     @IBOutlet weak var startDate: UILabel!
+    @IBOutlet weak var descriptionLbl: UILabel!
     
+    @IBOutlet weak var cerraduriaLbl: UILabel!
     
+    @IBOutlet weak var museografiaLbl: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         titleLbl.text = expo.title
         startDate.text = expo.startDate
+        descriptionLbl.text = expo.description
+        cerraduriaLbl.text = expo.cerraduria
+        museografiaLbl.text = expo.museografia
         
         // Do any additional setup after loading the view.
     }
