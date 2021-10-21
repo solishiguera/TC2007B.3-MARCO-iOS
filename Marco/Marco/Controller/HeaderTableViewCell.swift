ff64a0ba5427bcfd68a2a025e48abf416ada162e
Diego <A00827847@itesm.mx>
2021-10-21 00:35:48 -0500

    headers are hard to work with

---
+++ Marco/Marco/Controller/HeaderTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HeaderTableViewCell.swift
+//  Marco
+//
+//  Created by Diego Solis on 10/21/21.
+//
+
+import UIKit
+
+class HeaderTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var dateLbl: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
